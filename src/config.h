92a32481344e4945af34de83540685f5e1e00b69
Mike Campbell <mcampbell@weedmaps.com>
2025-06-22 17:40:15 -0700

    initial code

---
+++ src/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// SmartShunt device configuration
+// #define DEVICE_NAME "SmartShunt HQ2448HY62J"
+// #define DEVICE_PIN "171173" // Bonding PIN
+#define INSTANT_READOUT_ENCRYPTION_KEY "64cd146fe6771ef40610ecf50f3bb06a"
+#define INSTANT_READOUT_MAC_ADDRESS "d6ec4c9e6307"
+
+#endif // CONFIG_H çç
